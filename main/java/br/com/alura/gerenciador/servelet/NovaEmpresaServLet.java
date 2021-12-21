18643404ee8d5f1ff45571b77892d4a6a795228b
Ney Belchior <67557941+NeyBelchior@users.noreply.github.com>
2021-12-21 06:34:41 -0300

    doPost

--- main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServLet.java
+++ main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServLet.java
@@ -19,7 +19,7 @@ public class NovaEmpresaServLet extends HttpServlet {
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nomeDaEmpresa = request.getParameter("nome");
 		
 		
